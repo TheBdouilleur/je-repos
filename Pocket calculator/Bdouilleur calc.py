6281456dc0fe62ff3555817a66598b4a6b319c96
TheBdouilleur2 <52212434+TheBdouilleur2@users.noreply.github.com>
2019-06-29 11:31:31 +0200

    A new version: fonctionnal

--- Pocket calculator/Bdouilleur calc.py
+++ Pocket calculator/Bdouilleur calc.py
@@ -4,9 +4,10 @@ from tkinter import *
 calculator = Tk()
 
 # And personalize it
-calculator.geometry("415x320")
+calculator.geometry("415x405")
+calculator.resizable(0, 0)
 calculator.title('Pocket calculator for Bdouilleurs')
-calculator.iconbitmap('Pocket calculator.ico')
+calculator.iconbitmap('Pythonsignev.ico')
 calculator.config(background='green')
 
 can_button = Canvas(calculator, width=500, height=200, bg='black', bd=0, highlightthickness=0)
@@ -17,113 +18,158 @@ writing = Entry(can_entry, font=('Impossible', 30), bg='black', fg='green')
 writing.pack(expand=YES, fill=X)
 can_entry.pack(side=TOP, fill=X)
 writing.delete(0, END)
-global number
-number = StringVar()
-global plus
-plus = False
-global moins
-moins = False
 
-def Button_one():
+number = IntVar()
+
+number2 = IntVar()
+
+p = False
+moins = False
+mult = False
+div = False
+
+
+def zero_button(evt = None):
+    writing.insert(END, '0')
+
+
+def one_button(evt = None):
     writing.insert(END, '1')
 
 
-def Button_two():
+def two_button(evt = None):
     writing.insert(END, '2')
 
 
-def Button_tree():
+def three_button(evt = None):
     writing.insert(END, '3')
 
 
-def Button_four():
+def four_button(evt = None):
     writing.insert(END, '4')
 
 
-def Button_five():
+def five_button(evt = None):
     writing.insert(END, '5')
 
 
-def Button_six():
+def six_button(evt=None):
     writing.insert(END, '6')
 
 
-def Button_seven():
+def seven_button(evt = None):
     writing.insert(END, '7')
 
 
-def Button_eight():
+def eight_button(evt = None):
     writing.insert(END, '8')
 
 
-def Button_nine():
+def nine_button(evt = None):
     writing.insert(END, '9')
 
 
-def plus_button():
+def point_button(evt = None):
+    writing.insert(END, '.')
+
+
+def plus_button(evt = None):
+    global p
+    global number
     number = writing.get()
-    number = int(number)
-    print(number)
     writing.delete(0, END)
-    plus = True
+    p = True
 
 
-def moins_button():
+def moins_button(evt = None):
+    global moins
+    global number
     number = writing.get()
-    number = int(number)
-    print((number))
     writing.delete(0, END)
     moins = True
 
 
-def equal_button():
+def multiply_button(evt = None):
+    global mult
+    global number
+    number = writing.get()
+    writing.delete(0, END)
+    mult = True
+
+
+def division_button(evt = None):
+    global div
+    global number
+    number = writing.get()
+    writing.delete(0, END)
+    div = True
+
+
+def equal_button(evt = None): # TODO: entry key for equal
+    global p
+    global moins
+    global number
+    global mult
+    global div
     number2 = writing.get()
-    number2 = int(number2, 16)
-    print(number2)
+    number2 = float(number2)
+    number = float(number)
     result = 0
-    if plus:
-        result = number + number2
+    if p:
+        result =number + number2
+        p = False
     elif moins:
         result = number - number2
-    result = str(result)
-    print(result)
+        moins = False
+    elif mult:
+        result = number * number2
+        mult = False
+    elif div:
+        result = number / number2
+        div = False
+
     writing.delete(0, END)
     writing.insert(0, result)
 
 
-def clear_button():
+def clear_button(evt = None):
     writing.delete(0, END)
 
 
+button_zero = Button(can_button, text=' 0 ', font=('Impossible', 30), bg='green', fg='black', padx=15,
+                     command=zero_button)
 button_one = Button(can_button, text=' 1 ', font=('Impossible', 30), bg='green', fg='black', padx=15,
-                    command=Button_one)
+                    command=one_button)
 button_two = Button(can_button, text=' 2 ', font=('Impossible', 30), bg='green', fg='black', padx=15,
-                    command=Button_two)
+                    command=two_button)
 button_tree = Button(can_button, text=' 3 ', font=('Impossible', 30), bg='green', fg='black', padx=15,
-                     command=Button_tree)
+                     command=three_button)
 button_four = Button(can_button, text=' 4 ', font=('Impossible', 30), bg='green', fg='black', padx=15,
-                     command=Button_four)
+                     command=four_button)
 button_five = Button(can_button, text=' 5 ', font=('Impossible', 30), bg='green', fg='black', padx=15,
-                     command=Button_five)
+                     command=five_button)
 button_six = Button(can_button, text=' 6 ', font=('Impossible', 30), bg='green', fg='black', padx=15,
-                    command=Button_six)
+                    command=six_button)
 button_seven = Button(can_button, text=' 7 ', font=('Impossible', 30), bg='green', fg='black', padx=15,
-                      command=Button_seven)
+                      command=seven_button)
 button_eight = Button(can_button, text=' 8 ', font=('Impossible', 30), bg='green', fg='black', padx=15,
-                      command=Button_eight)
+                      command=eight_button)
 button_nine = Button(can_button, text=' 9 ', font=('Impossible', 30), bg='green', fg='black', padx=15,
-                     command=Button_nine)
+                     command=nine_button)
 button_plus = Button(can_button, text=' + ', font=('Impossible', 30), bg='green', fg='black', padx=15,
-                     textvariable=number, command=plus_button)
-button_moins = Button(can_button, text=' - ', font=('Impossible', 30), bg='green', fg='black', padx=15,
-                      command=moins_button)
-button_multiply = Button(can_button, text='  X  ', font=('Impossible', 30), bg='green', fg='black', padx=15)
-button_divise = Button(can_button, text=' / ', font=('Impossible', 30), bg='green', fg='black', padx=15)
+                     command=plus_button)
+button_moins = Button(can_button, text=' - ', font=('Impossible', 30), bg='green', fg='black', padx=15
+                      , command=moins_button)
+button_multiply = Button(can_button, text='  X  ', font=('Impossible', 30), bg='green', fg='black', padx=15,
+                         command=multiply_button)
+button_divise = Button(can_button, text=' / ', font=('Impossible', 30), bg='green', fg='black', padx=15,
+                       command=division_button)
 button_equal = Button(can_button, text=' = ', font=('Impossible', 30), bg='green', fg='black', padx=15,
                       command=equal_button)
 button_clear = Button(can_button, text=' C ', font=('Impossible', 30), bg='green', fg='black', padx=15,
                       command=clear_button)
-
+button_point = Button(can_button, text=' . ', font=('Impossible', 30), bg='green', fg='black', padx=15,
+                      command=point_button)
 
 button_one.grid(row=0)
 button_two.grid(row=0, column=1)
@@ -140,5 +186,27 @@ button_multiply.grid(row=1, column=3)
 button_divise.grid(row=1,column=4)
 button_equal.grid(row=2, column=3)
 button_clear.grid(row=2, column=4)
+button_zero.grid(row=3, column=1)
+button_point.grid(row=3, column=2)
+
+
+calculator.bind('1', one_button)
+calculator.bind('2', two_button)
+calculator.bind('3', three_button)
+calculator.bind('4', four_button)
+calculator.bind('5', five_button)
+calculator.bind('6', six_button)
+calculator.bind('7', seven_button)
+calculator.bind('8', eight_button)
+calculator.bind('9', nine_button)
+calculator.bind('0', zero_button)
+calculator.bind('c', clear_button)
+calculator.bind('C', clear_button)
+calculator.bind('.', point_button)
+calculator.bind('+', plus_button)
+calculator.bind('-', moins_button)
+calculator.bind('*', multiply_button)
+calculator.bind('/', division_button)
+
 
 calculator.mainloop()
